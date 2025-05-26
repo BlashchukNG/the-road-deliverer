c35d8b77ffd5a59b756876ef2dd9b763550e3806
BlashchukNG <54845130+BlashchukNG@users.noreply.github.com>
2025-05-26 13:13:38 +0300

    rework colliders

---
+++ Assets/_Project/Code/Logic/UserCamera/HideObject.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace Logic.UserCamera
+{
+	public sealed class HideObject : MonoBehaviour
+	{
+		[SerializeField] private MeshRenderer[] _renderers;
+
+		public void Hide()
+		{
+			foreach (var r in _renderers)
+				r.enabled = false;
+		}
+
+		public void Show()
+		{
+			foreach (var r in _renderers)
+				r.enabled = true;
+		}
+	}
+}
